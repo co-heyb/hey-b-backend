5acbffef4ba2be6a9889aad0d5470bf0b7de705e
dong-park <rakddh@gmail.com>
2025-04-02 21:52:44 +0900

    데이터베이스 설정을 SQLite에서 PostgreSQL로 변경 및 연결 정보 추가

--- heyb/settings.py
+++ heyb/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'heyb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.cqfbiiqjtymancflzqpr',
+        'PASSWORD': 'Park4023!@#!@#',
+        'HOST': 'aws-0-ap-northeast-2.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
