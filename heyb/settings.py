c38f8bc764524ac5f001b6f59ba8e5489d1f14bf
dong-park <rakddh@gmail.com>
2025-04-09 07:09:35 +0900

    정적 파일 경로 수정 및 배포 설정 업데이트: settings.py에서 STATIC_URL을 '/static/'에서 'static/'으로 변경하고, deployment.yaml에서 nginx 관련 설정을 제거하여 간소화함. kustomization.yaml에서 nginx-configmap.yaml을 리소스 목록에서 삭제하고, service.yaml에서 targetPort를 80에서 8000으로 수정하여 일관성을 유지함.

--- heyb/settings.py
+++ heyb/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 정적 파일 버전 추가 (배포 시 캐시 무효화)
